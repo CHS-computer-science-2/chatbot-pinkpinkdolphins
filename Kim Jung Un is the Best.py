fa4432265ce9e0e0b02837093040da986bba49eb
Eric Carlson <ecarlson0144@myduneland.org>
2019-04-01 08:39:43 -0500

    test

--- Kim Jung Un is the Best.py
+++ Kim Jung Un is the Best.py
@@ -1,2 +1,5 @@
-print "Welcome Comrade to OUR most glorious nation, that being the Democratic People's Republic of Korea,or as the filthy Imperialists call us, North Korea. We the only true legitimate state on the korean penninsula are a bastion of hope and prosperity to the people of Korea. WE are so glad that you have decided to become a part of our great country ruled by the great leader Kim Jung Un,but before we can let you in we'll have to make sure your not a western spy. Our brand new Comreade chat bot 3000 we'll ask you questions to make sure your truley going to be a great north korean citizen"
+print """Welcome Comrade to OUR most glorious nation, that being the Democratic People's Republic of Korea,
+or as the filthy Imperialists call us, North Korea. We the only true legitimate state on the korean penninsula are a bastion of hope and prosperity to the people of Korea.
+ WE are so glad that you have decided to become a part of our great country ruled by the great leader Kim Jung Un,but before we can let you in we'll have to make sure your not a western spy.
+  Our brand new Comreade chat bot 3000 we'll ask you questions to make sure your truley going to be a great north korean citizen"""
 from textblob import TextBlob
