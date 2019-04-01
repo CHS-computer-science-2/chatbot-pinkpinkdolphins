c28a26aae4d00945323ec19cb594d7cc6043ae27
Alec Malenfant <malenfantalec@gmail.com>
2019-04-01 08:36:37 -0500

    This will add a welcome message and an input

--- Kim Jung Un is the Best.py
+++ Kim Jung Un is the Best.py
@@ -1,3 +1,8 @@
 Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+>>>
+print("Welcome to KimBot!")
+print("This state of the art chatbot will give you the privelage of simulating a chat with our dear leader")
+print("on how to the be the best citizen of the Democratic People's Republic of Korea")
+
+input("\n:")
