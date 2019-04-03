8f421eee137f8595ae09263ccbec01e4510e9802
Alec Malenfant <malenfantalec@gmail.com>
2019-04-03 08:31:38 -0500

    Fix the beginning prompt

--- Kim Jung Un is the Best.py
+++ Kim Jung Un is the Best.py
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>>
-=======
-print """Welcome Comrade to OUR most glorious nation, that being the Democratic People's Republic of Korea,
-or as the filthy Imperialists call us, North Korea. We the only true legitimate state on the korean penninsula are a bastion of hope and prosperity to the people of Korea.
- WE are so glad that you have decided to become a part of our great country ruled by the great leader Kim Jung Un,but before we can let you in we'll have to make sure your not a western spy.
-  Our brand new Comreade chat bot 3000 we'll ask you questions to make sure your truley going to be a great north korean citizen"""
-from textblob import TextBlob
->>>>>>> fa4432265ce9e0e0b02837093040da986bba49eb
+#=======
+print("Welcome Comrade to OUR most glorious nation, that being the Democratic People's Republic of Korea,")
+print("\nor as the filthy Imperialists call us, North Korea. We the only true legitimate state on the korean penninsula are a bastion of hope and prosperity to the people of Korea.")
+print("\nWE are so glad that you have decided to become a part of our great country ruled by the great leader Kim Jung Un,but before we can let you in we'll have to make sure your not a western spy.")
+print("\nOur brand new Comreade chat bot 3000 we'll ask you questions to make sure your truley going to be a great north korean citizen")
+#import TextBlob
+#>>>>>>> fa4432265ce9e0e0b02837093040da986bba49eb
 
-userInput = input(": ")
+#input(":")
