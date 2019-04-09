1cc64f7b387ed6ac3ac79e3c39aa7fdd6669c906
Alec Malenfant <malenfantalec@gmail.com>
2019-04-09 08:01:31 -0500

    Adds Negative and Positive Keywords

--- Kim Jung Un is the Best.py
+++ Kim Jung Un is the Best.py
@@ -3,7 +3,10 @@ print("Welcome Comrade to OUR most glorious nation, that being the Democratic Pe
 print("\nor as the filthy Imperialists call us, North Korea. We the only true legitimate state on the korean penninsula are a bastion of hope and prosperity to the people of Korea.")
 print("\nWE are so glad that you have decided to become a part of our great country ruled by the great leader Kim Jung Un,but before we can let you in we'll have to make sure your not a western spy.")
 print("\nOur brand new Comreade chat bot 3000 we'll ask you questions to make sure your truley going to be a great north korean citizen")
-#import TextBlob
+from textblob import TextBlob
+
+negativeKeywords = ["America","Obama","Trump","President","Human Rights","Europe","Japan","Where's my mom?","Internet","South Korea", "Poverty","Seoul","Capitolism"]
+positiveKeywords = ["Dear Leader", "Kim", "Jong", "Un", "Il", "Sung","Russia", "North Korea", "Communism", "pyongyang"]
 #>>>>>>> fa4432265ce9e0e0b02837093040da986bba49eb
 
-#input(":")
+input(":")
