348f07ec0bb18b90dcda12d9710e926699488f9c
Alec Malenfant <malenfantalec@gmail.com>
2019-04-30 08:55:47 -0500

    Minor fixes

--- Kim Jung Un is the Best.py
+++ Kim Jung Un is the Best.py
@@ -1,9 +1,13 @@
-#=======
+#======= Welcome Message
 print("Welcome Comrade to OUR most glorious nation, that being the Democratic People's Republic of Korea,")
 print("\nor as the filthy Imperialists call us, North Korea. We the only true legitimate state on the korean penninsula are a bastion of hope and prosperity to the people of Korea.")
 print("\nWE are so glad that you have decided to become a part of our great country ruled by the great leader Kim Jung Un,but before we can let you in we'll have to make sure your not a western spy.")
 print("\nOur brand new Comrade chat bot 3000 we'll ask you questions to make sure your truley going to be a great north korean citizen")
+
+#Import random and textblob
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 from textblob import TextBlob
+import random
 
 # Groups of words
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -15,12 +19,12 @@ greetings = ["hello Comrade","hello","greetings"]
 comradeGreetings = ["Greetings Peasant!", "Greetings!", "WHAT DO YOU WANT????!!!!", "Go away", "*big sigh* hi"]
 
 #Functions
-#>>>>>>> fa4432265ce9e0e0b02837093040da986bba49eb
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 def greeting(sentence):
     for words in sentence.split():
         if words.lower() in greetings:
-            return(comradeGreetings[random.randint(0,5)
+            return(comradeGreetings[random.randint(0,5)])
         else:
             return("TRY AGAIN!!")
 
@@ -28,5 +32,6 @@ def greeting(sentence):
 
 #Main
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-userInput = input(":")
+userInput = input("\n: ")
 responce = greeting(userInput)
+print("Comrade : " + responce)
