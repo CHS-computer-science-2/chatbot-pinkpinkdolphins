e856fea750649dba5200c13e2e92c0288fece6ec
Alec Malenfant <malenfantalec@gmail.com>
2019-04-30 08:05:43 -0500

    Gets rid of old functions, adds greetings, and organizes code

--- Kim Jung Un is the Best.py
+++ Kim Jung Un is the Best.py
@@ -2,25 +2,23 @@
 print("Welcome Comrade to OUR most glorious nation, that being the Democratic People's Republic of Korea,")
 print("\nor as the filthy Imperialists call us, North Korea. We the only true legitimate state on the korean penninsula are a bastion of hope and prosperity to the people of Korea.")
 print("\nWE are so glad that you have decided to become a part of our great country ruled by the great leader Kim Jung Un,but before we can let you in we'll have to make sure your not a western spy.")
-print("\nOur brand new Comreade chat bot 3000 we'll ask you questions to make sure your truley going to be a great north korean citizen")
+print("\nOur brand new Comrade chat bot 3000 we'll ask you questions to make sure your truley going to be a great north korean citizen")
 from textblob import TextBlob
 
+# Groups of words
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 negativeKeywords = ["america","obama","trump","president","human rights","europe","japan","where's my mom?","internet","south korea", "poverty","seoul","capitalism"]
 positiveKeywords = ["dear leader", "Great Leader", "kim", "jong", "un", "il", "sung","russia", "north korea", "communism", "pyongyang"]
 questionKeywords = ["who","what","where","when","why","?"]
+
+greetings = ["hello Comrade","hello","greetings"]
+
+#Functions
 #>>>>>>> fa4432265ce9e0e0b02837093040da986bba49eb
 
+
+
+
+#Main
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 userInput = input(":")
-def checkPositiveNegative(userInput):
-    positive_amount=0
-    for x in response:
-        if x in positiveKeywords:
-            positive_amount+=1
-
-def checkPostitiveNegative(sentence):
-
-def sentenceParts(sentence):
-    print("I hope this works")
-    tags = sentence.tags
-    print(tags)
-    
