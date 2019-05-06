78ca556fbba361ec9a62fd15c599dfe854b2c96b
Alec Malenfant <malenfantalec@gmail.com>
2019-05-06 08:34:47 -0500

    CLEANING

--- Kim Jung Un is the Best.py
+++ Kim Jung Un is the Best.py
@@ -1,16 +1,22 @@
-import random
+#WELCOME STATEMENT>>>>>>>>>>>>>>>>>>>>>>>
 print("Welcome Comrade to OUR most glorious nation, that being the Democratic People's Republic of Korea,")
 print("\nor as the filthy Imperialists call us, North Korea. We the only true legitimate state on the korean penninsula are a bastion of hope and prosperity to the people of Korea.")
 print("\nWE are so glad that you have decided to become a part of our great country ruled by the great leader Kim Jung Un,but before we can let you in we'll have to make sure your not a western spy.")
 print("\nOur brand new Comreade chat bot 3000 we'll ask you questions to make sure your truley going to be a great north korean citizen")
+
+#IMPORTS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 from textblob import TextBlob
+import random
+
+#KEYWORDS>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 greeting_choice=random.randrange(0,4)
 negativeKeywords = ["america","obama","trump","president","human rights","europe","japan","where's my mom?","internet","south korea", "poverty","seoul","capitalism","food","38"]
 positiveKeywords = ["dear leader", "Great Leader", "kim", "jong", "un", "il", "sung","russia", "north korea", "communism", "pyongyang","Putin","xi jinping"]
 questionKeywords = ["who","what","where","when","why","?"]
 greetings=["Hello","What a fine day we are having","Praise our Glorious leader from whom all good in the world stems","Comrade Welcome","Are You a Filthy Capatalist Pig?? We\'ll find out",""]
-print (greeting[greeting_choice])
-userInput = input(":")
+
+#DEFINING FUNCTIONS>>>>>>>>>>>>>>>>>>>>>>
+
 #Checking the positive value of a word
 def check_positive(userInput):
     positive_amount=0
@@ -27,3 +33,8 @@ def check_negative(userinput):
         if x in positiveKeywords or x in questionKeywords:
             negative_amount+=1
             return negative_amount
+
+
+#MAIN >>>>>>>>>>>>>>>>>>>
+print (greetings[greeting_choice])
+userInput = input(":")
