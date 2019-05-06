3a6aaa0711fe55fcdae5e73a778d89f7462ef14d
Eric Carlson <ecarlson0144@myduneland.org>
2019-05-06 07:57:47 -0500

    Added negative response function

--- Kim Jung Un is the Best.py
+++ Kim Jung Un is the Best.py
@@ -1,37 +1,31 @@
-#======= Welcome Message
+#=======
 print("Welcome Comrade to OUR most glorious nation, that being the Democratic People's Republic of Korea,")
 print("\nor as the filthy Imperialists call us, North Korea. We the only true legitimate state on the korean penninsula are a bastion of hope and prosperity to the people of Korea.")
 print("\nWE are so glad that you have decided to become a part of our great country ruled by the great leader Kim Jung Un,but before we can let you in we'll have to make sure your not a western spy.")
-print("\nOur brand new Comrade chat bot 3000 we'll ask you questions to make sure your truley going to be a great north korean citizen")
-
-#Import random and textblob
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+print("\nOur brand new Comreade chat bot 3000 we'll ask you questions to make sure your truley going to be a great north korean citizen")
 from textblob import TextBlob
-import random
 
-# Groups of words
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-negativeKeywords = ["america","obama","trump","president","human rights","europe","japan","where's my mom?","internet","south korea", "poverty","seoul","capitalism"]
-positiveKeywords = ["dear leader", "Great Leader", "kim", "jong", "un", "il", "sung","russia", "north korea", "communism", "pyongyang"]
+negativeKeywords = ["america","obama","trump","president","human rights","europe","japan","where's my mom?","internet","south korea", "poverty","seoul","capitalism","food"]
+positiveKeywords = ["dear leader", "Great Leader", "kim", "jong", "un", "il", "sung","russia", "north korea", "communism", "pyongyang","Putin","xi jinping"]
 questionKeywords = ["who","what","where","when","why","?"]
+#>>>>>>> fa4432265ce9e0e0b02837093040da986bba49eb
 
-greetings = ["hello Comrade","hello","greetings"]
-comradeGreetings = ["Greetings Peasant!", "Greetings!", "WHAT DO YOU WANT????!!!!", "Go away", "*big sigh* hi"]
+userInput = input(":")
+def checkPositiveNegative(userInput):
+    positive_amount=0
+    userinput=userInput.lower
+    for x in userInput:
+        if x in positiveKeywords:
+            positive_amount+=1
+def checknegative(userinput):
+    negative_amount=0
+    userinput=userInput.lower
+    for x in userInput:
+        if x in positiveKeywords:
+            negative_amount+=1
 
-#Functions
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-def greeting(sentence):
-    for words in sentence.split():
-        if words.lower() in greetings:
-            return(comradeGreetings[random.randint(0,5)])
-        else:
-            return("TRY AGAIN!!")
 
-#
+def checkPostitiveNegative(sentence):
 
-#Main
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-userInput = input("\n: ")
-responce = greeting(userInput)
-print("Comrade : " + responce)
+def sentenceParts(sentence):
